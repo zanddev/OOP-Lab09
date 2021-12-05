1d8e5949d8b5a0615922bc51e9d8b55c8945cc47
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-12-05 17:08:51 +0100

    fix: removed unused import

--- src/it/unibo/oop/lab/lambda/ex01/LambdaUtilities.java
+++ src/it/unibo/oop/lab/lambda/ex01/LambdaUtilities.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
